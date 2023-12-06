b6e029126be17f2110d92142320588e78dbdeba2
Arthur Pankiewicz <arthur@bitovi.com>
2023-12-05 16:51:02 -0800

    feat: count imports, handle multiple repos, inputs

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,80 @@
 import { join } from "node:path";
 import { glob } from "glob";
-import { readFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import { getImports } from "./get-imports";
 import { gitClone } from "./git-clone";
 import { getTempDir } from "./get-temp-dir";
 
 main();
 
+interface Input {
+  imports: string[];
+  library: string;
+  repos: Array<{
+    name: string;
+    git?: {
+      hosting?: "github" | "bitbucket";
+      protocol?: "ssh" | "https";
+      author?: string;
+      branch?: string;
+    };
+  }>;
+  git: {
+    username: string;
+    author: string;
+    hosting: "github" | "bitbucket";
+    protocol: "ssh" | "https";
+    branch: string;
+  };
+}
+
 async function main() {
-    const dir = getTempDir();
+  const dir = getTempDir();
 
-    const projectName = "eslint-plugin";
+  const { imports, library, repos, git }: Input = JSON.parse(
+    readFileSync(__dirname + "/../input.json", "utf8")
+  );
 
-    const projectPath = join(dir, projectName);
+  const output = {
+    metadata: {
+      date: new Date().toISOString().split("T")[0],
+    },
+  };
 
-    const cleanup = await gitClone({ branchName: 'master', projectPath, projectName, author: 'bitovi', hosting: "github", username: "m-thompson-code", protocol: "https" });
+  await Promise.all(
+    repos.map(async ({ name: repo, git: gitOverride }) => {
+      const projectPath = join(dir, repo);
 
-    const ext = ["ts"];
-    const filePaths = await glob(join(projectPath, `/**/*.@(${ext.join("|")})`));
+      const cleanup = await gitClone({
+        branchName: gitOverride?.branch || git.branch,
+        projectPath,
+        projectName: repo,
+        author: gitOverride?.author || git.author,
+        hosting: gitOverride?.hosting || git.hosting,
+        username: git.username,
+        protocol: gitOverride?.protocol || git.protocol,
+      });
 
-    console.log(filePaths);
-    const imports = filePaths
+      const ext = ["ts"];
+      const filePaths = await glob(
+        join(projectPath, `/**/*.@(${ext.join("|")})`)
+      );
+
+      const matchedImports = filePaths
         .map((filePath) => readFileSync(filePath, "utf8"))
-        .map((file) => getImports(file, "@angular/core"))
-        .flat();
+        .map((file) => getImports(file, imports, library))
+        .flat()
+        .reduce((acc, curr) => {
+          if (!acc[curr]) acc[curr] = { count: 0 };
+          acc[curr].count += 1;
+          return acc;
+        }, {});
 
-    console.log(imports);
+      cleanup();
 
-    cleanup();
+      output[repo] = matchedImports;
+    })
+  );
+
+  writeFileSync(__dirname + "/../output.json", JSON.stringify(output, null, 2));
 }
