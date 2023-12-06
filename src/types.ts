30cb36fc140ea55304d21d6cc77af858166c50a6
Arthur Pankiewicz <arthur@bitovi.com>
2023-12-06 14:14:09 -0800

    track repos that include/exclude imports

--- src/types.ts
+++ src/types.ts
@@ -1,44 +1,44 @@
 export interface GitOptions {
-    username: string;
-    owner: string;
-    hosting: "github" | "bitbucket";
-    protocol: "ssh" | "https";
-    branch: string;
+  username: string;
+  owner: string;
+  hosting: "github" | "bitbucket";
+  protocol: "ssh" | "https";
+  branch: string;
 }
 
 export interface GitCloneOptions extends GitOptions {
-    // hosting: 'bitbucket' | 'github';
-    // protocol: 'ssh' | 'https';
-    repoName: string;
-    repoPath: string;
-    // owner: string;
-    // username: string;
-    // branch: string;
+  // hosting: 'bitbucket' | 'github';
+  // protocol: 'ssh' | 'https';
+  repoName: string;
+  repoPath: string;
+  // owner: string;
+  // username: string;
+  // branch: string;
 }
 
 export interface Repo {
-    name: string;
-    git?: Partial<GitOptions>;
+  name: string;
+  git?: Partial<GitOptions>;
 }
 
 export interface Input {
-    imports: string[];
-    library: string;
-    repos: Array<Repo>;
-    git: GitOptions;
+  imports: string[];
+  library: string;
+  repos: Array<Repo>;
+  git: GitOptions;
 }
 
 export interface Output {
-    metadata: {
-        date: string;
-        reposThatIncludeImports: number;
-        reposThatExcludesImports: number;
+  metadata: {
+    date: string;
+    reposThatIncludeImports: string[];
+    reposThatExcludesImports: string[];
+  };
+  repos: {
+    [repoName: string]: {
+      importsUsed: number;
+      importsNotUsed: number;
+      imports: Record<string, { count: number }>;
     };
-    repos: {
-        [repoName: string]: {
-            importsUsed: number;
-            importsNotUsed: number;
-            imports: Record<string, { count: number }>;
-        };
-    };
+  };
 }
